dff85c7e15538f4f64b0a621f76b8518ea3b0f85
hisaki <alltimelow2011@gmail.com>
2023-02-11 12:44:13 +0900

    fix: rebuilt plans controller

--- spec/requests/plans_spec.rb
+++ spec/requests/plans_spec.rb
@@ -2,6 +2,45 @@ require 'rails_helper'
 
 RSpec.describe "Plans", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "returns http success" do
+      get "/plans/index"
+      expect(response).to have_http_status(:success)
+    end
   end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/plans/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/plans/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/plans/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/plans/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/plans/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
