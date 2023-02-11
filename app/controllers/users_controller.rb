939fcb7e8236d1c492c057ce21e7df5d047cac58
hisaki <alltimelow2011@gmail.com>
2023-02-11 10:35:38 +0900

    add: logout done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect to login_path, notice: 'successfully sign up'
+      redirect_to login_path, notice: 'successfully sign up'
     else
       flash[:alert] = 'failed to signup'
-      render:new
+      render :new
     end
   end
 
   private
 
   def user_params
-    params.require(:users).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
