bccde6e99d17e7805695dea6ba899904aed09b8f
hisaki <alltimelow2011@gmail.com>
2023-02-13 16:02:46 +0900

    fix: schema.rb

--- db/migrate/20230211060810_create_informations.rb
+++ db/migrate/20230211060810_create_informations.rb
@@ -1,18 +1,14 @@
 class CreateInformations < ActiveRecord::Migration[7.0]
   def change
     create_table :informations do |t|
-      t.integer :country_area_cd
       t.integer :country_cd
       t.string :country_name
       t.string :measure_url
-      t.text :measure_lead
-      t.text :measure_subtext
-      t.text :measure_main1
-      t.text :measure_main2
-      t.text :measure_main3
-      t.text :measure_main4
-      t.text :measure_main5
-      t.text :measure_main6
+      t.text :measure_text2
+      t.text :measure_text3
+      t.text :measure_text4
+      t.text :measure_text5
+      t.text :measure_text6
       
       t.timestamps
     end
