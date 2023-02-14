06b93f8496d5923437e3a0a87a301460c2163d99
hisaki <alltimelow2011@gmail.com>
2023-02-14 11:11:47 +0900

    add: geocoding sample is changed

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,11 +3,11 @@ Geocoder.configure(
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :nominatim,         # name of geocoding service (symbol)
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
-  # language: :en,              # ISO-639 language code
+  language: :ja,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
+  api_key: "GOOGLE_API_KEY",               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
 
   # Exceptions that should not be rescued by default
