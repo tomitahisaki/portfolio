25991d84c246bfb84ae35b39eb155821cbc8c904
hisaki <alltimelow2011@gmail.com>
2023-02-15 09:57:47 +0900

    add: rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_060810) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_15_001640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "informations", force: :cascade do |t|
     t.integer "country_cd"
