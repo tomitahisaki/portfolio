d31b3377f9dc68be0b2a456178a1ac3e34e58385
hisaki <alltimelow2011@gmail.com>
2023-02-15 09:23:35 +0900

    add: plans/index depulicate to plans/new and rails g model countries

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,3 @@
 class Plan < ApplicationRecord
+   
 end
---
+++ db/migrate/20230215001640_create_countries.rb
@@ -0,0 +1,11 @@
+class CreateCountries < ActiveRecord::Migration[7.0]
+  def change
+    create_table :countries do |t|
+      t.string :name, null: false
+      t.float :latitude, null: false
+      t.float :longitude, null: false
+
+      t.timestamps
+    end
+  end
+end
