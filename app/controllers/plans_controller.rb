67f983402ce0d120445e08703759a0f9aebe89ea
hisaki <alltimelow2011@gmail.com>
2023-03-06 13:07:26 +0900

    add: add assosiation method to informations_table in  create and update action of plans_controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -15,21 +15,20 @@ class PlansController < ApplicationController
     
     @plan.transaction do
       countries = plan_params[:countries_attributes].values.map do |country_params| # フォームから送信された値のplan_paramsのcountry_attributesのハッシュの値(value)をmapメソッドで配列に変換し、countries変数に入れる。
-        Country.find_or_initialize_by(name: country_params[:name]) do |country| # Countryモデルから、country_params[:name]でフォームで受け取った値があるか確認する。ある場合はfind_byとなる。ない場合は、initializeで新規作成が行われる。
-          country.assign_attributes(country_params.except(:_destroy))
+                    Country.find_or_create_by(name: country_params[:name]) do |country| # Countryモデルから、country_params[:name]でフォームで受け 取った値があるか確認する。ある場合はfind_byとなる。ない場合は、createで新規作成と保存が行われる。
+                      country.assign_attributes(country_params.except(:_destroy))
+                    end
+                  end
+
+      countries.each do |country|
+        set_info = Information.where("country_name LIKE ?", "%#{country.name}%")  # country.nameを渡して、country_nameに部分一致するものを取り出す。
+        if set_info.present? && set_info.pluck(:country_id).nil?                  # set_infoに情報があり、かつcountry_id(外部キー)がない場合に以下の処理をする。
+          set_info.update(country_id: country.id)                                 # 外部キーのcountry_idにcountry.idを入れて更新する。
         end
+        binding.break
       end
-
-      @plan.countries = countries
-
-      # countries.each do |country|
-      #   @set_info = Information.find_by("country_name LIKE ?", "#%{country.name}%")
-      #   if @set_info.present?
-      #     @set_info.save(country_id: country.id)
-      #   end
-      # end
       
-      binding.break
+      @plan.countries = countries
       if @plan.save
         redirect_to plans_path, success: 'success'
       else
@@ -53,8 +52,15 @@ class PlansController < ApplicationController
 
     @plan.transaction do
       countries = plan_params[:countries_attributes].values.map do |country_params| # フォームから送信された値のplan_paramsのcountry_attributesのハッシュの値(value)をmapメソッドで配列に変換し、countries変数に入れる。
-        Country.find_or_initialize_by(name: country_params[:name]) do |country| # Countryモデルから、country_params[:name]でフォームで受け取った値があるか確認する。ある場合はfind_byとなる。ない場合は、initializeで　新規作成が行われる。
-          country.assign_attributes(country_params.except(:_destroy))
+                    Country.find_or_create_by(name: country_params[:name]) do |country| # Countryモデルから、country_params[:name]でフォームで受け取った値があるか確認する。ある場合はfind_byとなる。ない場合は、initializeで　新規作成が行われる。
+                      country.assign_attributes(country_params.except(:_destroy))
+                    end
+                  end
+
+      countries.each do |country|
+        set_info = Information.where("country_name LIKE ?", "%#{country.name}%")
+        if set_info.present? && set_info.pluck(:country_id).nil?
+          set_info.update(country_id: country.id)
         end
       end
 
