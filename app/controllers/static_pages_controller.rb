e9702f164f7df15b36958045afea60d4c13152e4
hisaki <alltimelow2011@gmail.com>
2023-03-12 19:43:04 +0900

    fix: static_page_controller is changed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: [:top]
+  skip_before_action :require_login, only: %i[top term policy contact ]
 
   def top; end
+  
+  def term; end
+  
+  def policy; end
+  
+  def contact; end
+
 end
