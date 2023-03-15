07299b3c8964ac5a1a359be851dde3772cbba724
hisaki <alltimelow2011@gmail.com>
2023-03-15 20:31:27 +0900

    add: add ja.yml to view and activerecord

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,8 @@ module Portfolio
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # Don't generate system test files.
     config.generators.system_tests = nil
