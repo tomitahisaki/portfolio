37c8a8c4b0efca47db174ba0c7b1f7ddfd997770
hisaki <alltimelow2011@gmail.com>
2023-03-18 10:39:14 +0900

    add: i18n views/ja.yml

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: [:top]
+  skip_before_action :require_login, only: %i[top contact term policy]
 
   def top; end
+  def contact; end
+  def term; end
+  def policy; end
 end
