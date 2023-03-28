36121f3916daa2c19617b65172e1c5069638f6a1
hisaki <alltimelow2011@gmail.com>
2023-03-28 16:44:10 +0900

    fix plans_countries/show design is changed

--- app/controllers/plan_countries_controller.rb
+++ app/controllers/plan_countries_controller.rb
@@ -1,9 +1,11 @@
 class PlanCountriesController < ApplicationController
+  
   def show
     @plan = Plan.find(params[:plan_id])
     @countries =  @plan.countries
     gon.countries = @countries
     gon.country_information = @countries.map(&:information)
     gon.measure_text2 = I18n.t('activerecord.attributes.information.measure_text2')
+    
   end
 end
