93345fa9631020521867f49cd8a91f47d2932c2f
hisaki <alltimelow2011@gmail.com>
2023-04-08 12:51:24 +0900

    add: create budget_category table

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,3 +1,5 @@
 class Budget < ApplicationRecord
   belongs_to :plan_country
+  has_many :budget_categories, dependent: :destroy
+  has_many :categories, through: :budget_categories
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :budget_categories, dependent: :destroy
+  has_many :budgets, through: :budget_categories
 end
